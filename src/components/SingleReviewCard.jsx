dbab73b83f4649f801aad6c0a6049bf3754ccd1a
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2024-12-07 14:30:44 +0600

    added filter and sorting features and solved some issues

--- src/components/SingleReviewCard.jsx
+++ src/components/SingleReviewCard.jsx
@@ -28,14 +28,15 @@ const SingleReviewCard = ({ review }) => {
          </div>
          <Divider sx={{ borderColor: "gray", my: 2 }} />
          <div className='flex gap-3 md:gap-5'>
-            <div className='w-56'>
+            <div className='flex-1'>
                <img
                   src={thumbnailURL}
                   alt={gameName}
-                  className='rounded-md aspect-square'
+                  className='rounded-md aspect-square object-cover'
                />
             </div>
-            <div>
+
+            <div className='flex-[2]'>
                <h6 className='text-lg font-semibold mb-2'>{gameName}</h6>
                <a
                   id={`rating-${_id}`}
@@ -53,7 +54,11 @@ const SingleReviewCard = ({ review }) => {
                   anchorSelect={`#rating-${_id}`}
                   content={`${rating} out of 5`}
                />
-               <p>{userReview.slice(0, 100)}....</p>
+               <p>
+                  {userReview.length > 100
+                     ? `${userReview.slice(0, 100)}....`
+                     : userReview}
+               </p>
                <Link to={`/review/${_id}`}>
                   <button className='px-4 py-2 bg-primary text-neutral rounded-sm mt-3 focus:scale-95'>
                      Explore Details
--- src/pages/AllReviews.jsx
+++ src/pages/AllReviews.jsx
@@ -6,36 +6,61 @@ import Loader from "../components/Loader";
 const AllReviews = () => {
    const [allReviews, setAllReviews] = useState([]);
    const [loadingAllReviews, setLoadingAllReviews] = useState(true);
+   const [filterBy, setFilterBy] = useState("");
+   const [sortWith, setSortWith] = useState("");
    useEffect(() => {
       setTimeout(() => {
          axios
-            .get("https://chill-gamer-backend.vercel.app/reviews")
+            .get(
+               `https://chill-gamer-backend.vercel.app/reviews?filterBy=${filterBy}&sortBy=${sortWith}`
+            )
             .then((data) => setAllReviews(data.data))
             .then(() => setLoadingAllReviews(false));
       }, 1000);
-   }, []);
+   }, [filterBy, sortWith]);
+   const handleFiltering = (e) => setFilterBy(e.target.value);
+   const handleSorting = (e) => setSortWith(e.target.value);
+
    if (loadingAllReviews) return <Loader />;
    return (
       <div className='my-10'>
          <div className='md:flex justify-between items-center'>
             <h2 className='font-semibold text-4xl'>All Reviews!!</h2>
-            <div className='text-right'>
+            <div className='text-right space-x-2'>
                <select
                   name='filterBy'
+                  className='bg-transparent *:text-gray-800 border p-1 md:p-2 outline-none rounded-sm mt-3'
+                  onChange={handleFiltering}>
+                  <option value='filterBy' disabled selected>
+                     Filter by Genres..
+                  </option>
+                  <option value='action'>Action</option>
+                  <option value='RPG'>RPG</option>
+                  <option value='adventure'>Adventure</option>
+                  <option value='strategy'>Strategy</option>
+                  <option value='simulation'>Simulation</option>
+               </select>
+               <select
+                  name='sortBy'
+                  onChange={handleSorting}
                   className='bg-transparent *:text-gray-800 border p-1 md:p-2 outline-none rounded-sm mt-3'>
-                  <option value='filter-by' disabled selected>
-                     Filter By..
+                  <option value='' disabled selected>
+                     Sort by..
                   </option>
                   <option value='rating'>Rating</option>
-                  <option value='rating'>Publishing Year</option>
+                  <option value='year'>Publishing Year</option>
                </select>
             </div>
          </div>
-         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5'>
-            {allReviews.map((review) => (
-               <SingleReviewCard review={review} key={review._id} />
-            ))}
-         </div>
+         {allReviews.length > 0 ? (
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5'>
+               {allReviews.map((review) => (
+                  <SingleReviewCard review={review} key={review._id} />
+               ))}
+            </div>
+         ) : (
+            <h4 className='mt-5'> No data found</h4>
+         )}
       </div>
    );
 };
